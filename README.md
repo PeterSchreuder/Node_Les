873685d42630c1a29e289ebce1558a113bd4bfcc
DESKTOP-15UVG1H\Peter <peter.schreuder@quicknet.nl>
2018-03-16 14:06:12 +0100

    seccond commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+"# Node_Les"
+
+
+Hier zit de opdracht voor de les van NODE
 "# Node_Les" 
---
+++ hallo.js
@@ -0,0 +1,14 @@
+
+
+console.time('100-elements');
+for (let i = 1; i < 101; i++)
+{
+  console.log("Hallo Wereld "+i+"x");
+}
+console.timeEnd('100-elements');
+
+
+const count = 5;
+console.log('count: %d', count);
+
+console.log('count:', count);
